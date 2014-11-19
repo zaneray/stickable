a58dbbebc1b9eaed07af1aa80c4f3aa8f606ec54
Jeremiah Martin <jer@jeremiahmartin.com>
2014-11-19 16:40:28 -0700

    full effin overhaul

--- js/stickable.js
+++ js/stickable.js
@@ -11,8 +11,6 @@
       stickyBreakpoint : 768 // Anything below this number stickable is disabled
     };
 
-
-
     $.extend(options, args);
     if (options.bottomId === null) {
       alert('Please specify a bottomId');
@@ -27,53 +25,73 @@
       options.marginBottom = options.margin;
     }  
 
-    var $el = this, 
-      $elParent = $el.parent(),
-      $window = $(window), 
-      $bottom = $('#' + options.bottomId), 
-      windowTop = $window.scrollTop(), 
-      stickyActive = false, 
-      fixedTop = true, 
-      winHeight = $window.height(), 
-      winWidth = $window.width(),  
-      elOffset = $elParent.offset().top, 
-      elHeight = $el.outerHeight(),
-      fixedStart = elOffset + elHeight - winHeight + options.marginBottom; 
-      bottomOffset = $bottom.offset().top,
-      isTouch      = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+    var $el                 = this, 
+        $elParent           = $el.parent(),
+        $window             = $(window), 
+        $bottom             = $('#' + options.bottomId), 
+        windowTop           = $window.scrollTop(), 
+        stickyActive        = false, 
+        fixedTop            = true, 
+        winHeight           = $window.height(), 
+        winWidth            = $window.width(),  
+        elOffset            = $elParent.offset().top, 
+        elHeight            = $el.outerHeight(),
+        //fixedStart        = elOffset + elHeight - winHeight + options.marginBottom,
+        dynamicTopPosition  = 0,
+        bottomOffset        = $bottom.offset().top,
+        downStart           = elOffset +  dynamicTopPosition + elHeight + options.marginBottom - winHeight,
+        downEnd             = bottomOffset - winHeight,    
+        upStart             = dynamicTopPosition + elOffset + options.marginTop,
+        upEnd               = elOffset,    
+        //fixedStatus         = 'static', //keep track of the status values: top, bottom, absolute, or static
+        scrollDirection     = 'down', //alternately it can be up
+        isTouch             = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+
 
     var destroySticky = function() {
       //note don't set any other inline styles on this baby or it will screw everything up
       stickyActive = false;
-      $el.removeAttr('style').removeClass("stickable-fixed-bottom").removeClass("stickable-fixed-top").removeClass("stickable-fixed");
+      $el.removeAttr('style').removeClass("stickable-absolute").removeClass("stickable-fixed-top").removeClass("stickable-fixed");
       $elParent.removeClass("stickable-parent");
       $window.off('scroll', scrollSticky);
     };
-      
-    var wordcheck = null;
+
 
     /*method that can reset all the demensions for the plugin. useful if anything changes the 
     dimensions of the page or the sticky Element. */
-    $.fn.stickable.reset = function(newBottom) {
+    $.fn.stickable.reset = function() {
       elHeight = $el.outerHeight();
       elOffset = $el.parent().offset().top;
-      if ( newBottom != null ) {
-        bottomOffset = $("#"+newBottom).offset().top;
-      }
-      else{
-        bottomOffset = $bottom.offset().top; 
-      }
-      fixedStart = elOffset + elHeight - winHeight + options.marginBottom;
+      bottomOffset = $bottom.offset().top; 
+      dynamicTopPosition = $el.offset().top - elOffset;
+      upStart = dynamicTopPosition + elOffset + options.marginTop;
+      upEnd = elOffset;
+      downStart = elOffset + elHeight + dynamicTopPosition + options.marginBottom - winHeight;
+      downEnd = bottomOffset - winHeight;  
     };
 
     /*method to destroy stickable altogether */
     $.fn.stickable.destroy = function() {
       destroySticky();
-    }
+    };
 
-    if (this.length > 0) {
+    if (this.length > 0) { //check if it exists. 
       
       var scrollSticky = function() { 
+        //elTopPosition needs to be scoped to this because we have multiple elements that can be sticky.
+        var elTopPosition = parseInt(bottomOffset - elHeight - elOffset - options.marginBottom);
+        //before resetting windowTop we compare them to figure out the scroll direction down/up
+        // console.log('----------------------------------');
+        // console.log('$window.scrollTop(): ' + $window.scrollTop());
+        // console.log('elTopPosition: ' + elTopPosition);
+        // console.log('downStart: ' + downStart);
+        if($window.scrollTop() > windowTop) {
+          scrollDirection = 'down';
+        }
+        else {
+          scrollDirection = 'up';
+        }
+        //console.log('scrollDirection:' + scrollDirection);
         windowTop = $window.scrollTop();
         if (fixedTop === true) {
           /********************************************
@@ -82,42 +100,120 @@
            than the window
            ********************************************/
           if (elOffset < windowTop + options.marginTop) {
-            if ((windowTop + elHeight + options.marginTop + options.marginBottom) > bottomOffset) {
-              $el.addClass('stickable-fixed-bottom').removeClass('stickable-fixed-top').css({
-                'top': parseInt(bottomOffset - elHeight - elOffset - options.marginBottom) + 'px',
+            if (downStart > bottomOffset) {
+              $el.addClass('stickable-absolute').removeClass('stickable-fixed-top').removeClass('stickable-fixed-bottom').css({
+                'top': elTopPosition + 'px',
                 'bottom': 'auto'
               });
+              //console.log('FIXED TOP \ndownStart > bottomOffset\n stickable-absolute \n top: ' + elTopPosition + ' \n bottom: auto \n ===============');
             } else { 
-              $el.addClass('stickable-fixed-top').removeClass('stickable-fixed-bottom').css('top', options.marginTop + 'px');
+              $el.addClass('stickable-fixed-top').removeClass('stickable-absolute').removeClass('stickable-fixed-bottom').css({
+                'top': options.marginTop + 'px',
+                'bottom': 'auto'
+              });
+              //console.log('FIXED TOP \ndownStart < bottomOffset\n stickable-fixed-top \n top: options.marginTop \n bottom: auto \n ===============');
             }
           } else {
-            $el.removeClass('stickable-fixed-top').removeClass('stickable-fixed-bottom').css('top', 'auto');
+            $el.removeClass('stickable-fixed-top').removeClass('stickable-absolute').removeClass('stickable-fixed-bottom').css({
+              'top' : 'auto',
+              'bottom' : 'auto'
+            });
+            //console.log('FIXED TOP \n remove stickable-fixed-bottom,stickable-absolute \n top: auto \n bottom: auto \n ===============');
           }
         } else {
           /********************************************
            FIXED BOTTOM
-           fix  to the bottom if it is taller
-           than the window
+           can fix  to the bottom if it is taller
+           than the window 
            ********************************************/
-          //fix  to the bottom if it is taller than the window
-          if (fixedStart < windowTop) {
-            if (windowTop + winHeight > bottomOffset) {
-                $el.addClass('stickable-fixed-bottom').removeClass('stickable-fixed-top').css({
-                  'top': parseInt(bottomOffset - elHeight - elOffset - options.marginBottom) + 'px',
+          if(scrollDirection === 'down') {
+            dynamicTopPosition = $el.offset().top - elOffset;
+            upStart = dynamicTopPosition + elOffset + options.marginTop;
+            //console.log('dynamicTopPosition: ' + dynamicTopPosition);
+            //going down
+            if(windowTop < downStart) {
+              $el.addClass('stickable-absolute')
+                .removeClass('stickable-fixed-top')
+                .removeClass('stickable-fixed-bottom')
+                .css({
+                  'top': dynamicTopPosition + 'px',
                   'bottom': 'auto'
                 });
-            } else {
-              $el.addClass('stickable-fixed').removeClass('stickable-fixed-bottom').css('top', 'auto').css('bottom', options.marginBottom + 'px');
+                //console.log('windowTop < downStart \n stickable-absolute \n top: ' + dynamicTopPosition + ' \n bottom: auto\n ===============');
+            } else 
+            if (windowTop > downStart && windowTop < downEnd) {
+              $el.addClass('stickable-fixed-bottom')
+                .removeClass('stickable-fixed-top')
+                .removeClass('stickable-absolute')
+                .css({
+                  'top': 'auto',
+                  'bottom': options.marginBottom + 'px'
+                });
+              //console.log('stickable-fixed-bottom \n top auto \n bottom: options.marginBottom\n ===============');
+            } else 
+            if (windowTop > downEnd) {
+              $el.addClass('stickable-absolute')
+                  .removeClass('stickable-fixed-top')
+                  .removeClass('stickable-fixed-bottom')
+                  .css({
+                    'top': elTopPosition + 'px',
+                    'bottom': 'auto'
+                  });
+                  //fixedStatus = 'bottom';
+                //console.log('stickable-absolute \n top: ' + elTopPosition + ' \n bottom: auto\n ===============');
             }
-          } else {
-            $el.removeClass('stickable-fixed').removeClass('stickable-fixed-bottom').css({
-              'top' : 'auto',
-              'bottom' : 'auto'
-            });
+            else {
+              //console.log('WTF should be no way to get here');
+            }
+          }
+          else {
+            //going up
+            dynamicTopPosition = $el.offset().top - elOffset;
+            downStart = elOffset + elHeight + dynamicTopPosition + options.marginBottom - winHeight;
+            
+            if (windowTop < upEnd) {
+              $el.addClass('stickable-absolute')
+                .removeClass('stickable-fixed-top')
+                .removeClass('stickable-fixed-bottom')
+                .css({
+                  'top': 0 + 'px',
+                  'bottom': 'auto'
+                });
+                //console.log('windowTop < upEnd \n stickable-absolute \n top: ' + options.marginTop + ' \n bottom: auto\n ===============');
+            } else 
+            if (windowTop  > upEnd && windowTop < upStart - (options.marginTop * 2)) {
+              $el.addClass('stickable-fixed-top')
+                .removeClass('stickable-absolute')
+                .removeClass('stickable-fixed-bottom')
+                .css({
+                  'top': options.marginTop + 'px',
+                  'bottom': 'auto'
+                });
+
+
+
+                //fixedStatus = 'bottom';
+              //console.log('windowTop > upEnd && windowTop < upStart \n stickable-fixed-top \n top: ' + options.marginTop + ' \n bottom: auto\n ===============');
+            } else 
+            if (windowTop > upStart) {
+              $el.addClass('stickable-absolute')
+                .removeClass('stickable-fixed-top')
+                .removeClass('stickable-fixed-bottom')
+                .css({
+                  'top': dynamicTopPosition + 'px',
+                  'bottom': 'auto'
+                });
+                  //fixedStatus = 'bottom';
+                //console.log('windowTop > upStart \n stickable-absolute \n top: ' + dynamicTopPosition + ' \n bottom: auto\n ===============');
+            }
+
+
+            
           }
         }
       };
       //end scrollSticky
+
 
       //initSticky() function that initializes stickable
       var initSticky = function() {
@@ -125,12 +221,19 @@
         $elParent.addClass('stickable-parent');
         $el.outerWidth($elParent.outerWidth());
         if ((elHeight + (options.marginTop  + options.marginBottom)) < winHeight) {
+          //bigger than the window
           fixedTop = true;
         } else {
+          //smaller than the window
           fixedTop = false;
         }
 
         $window.on('scroll', scrollSticky);
+
+        //if we're scrolled down the page trigger a scroll so that it sets the sticky in the right place
+        if(windowTop > 0) {
+          $window.scrollTop(windowTop + 1);
+        }
       };
       //end initSticky();
 
@@ -145,39 +248,42 @@
             initSticky();
           } else {
             //just resizing no biggie            
-            $el.width($elParent.width());
+            $el.outerWidth($elParent.width());
           }
         } else if (winWidth < options.stickyBreakpoint && stickyActive === true) {
           destroySticky();
         }
       };
+
+      //do this on $window.load(); if the window Width is larger than the sticky breakpoint fire it up. 
+      if (winWidth >= options.stickyBreakpoint) {
+        //continue we are at a larger screen
+        initSticky();
+      }
+
+      //if the page loads and the window is already scrolled down call the scroll function.
+      if(windowTop > 0) {
+        scrollSticky();
+      }
+
+      //resize with a simple throttling function built in.
+      if(isTouch){
+        var resizeTimeout;
+        $window.on('resize orientationchange', function(){
+          clearTimeout(resizeTimeout);
+          resizeTimeout = setTimeout(stickyResize, 200);
+        });
+        
+      }
+      else {      
+        $window.on('resize', function(){
+          stickyResize();  
+        });      
+      }
+
     } // end if (this.length > 0)
 
-    //do this on $window.load(); if the window Width is larger than the sticky breakpoint fire it up. 
-    if (winWidth >= options.stickyBreakpoint) {
-      //continue we are at a larger screen
-      initSticky();
-    }
-
-    //if the page loads and the window is already scrolled down call the scroll function.
-    if(windowTop > 0) {
-      scrollSticky();
-    }
-
-    //resize with a simple throttling function built in.
-    if(isTouch){
-      var resizeTimeout;
-      $window.on('resize orientationchange', function(){
-        clearTimeout(resizeTimeout);
-        resizeTimeout = setTimeout(stickyResize, 200);
-      });
-      
-    }
-    else {      
-      $window.on('resize', function(){
-        stickyResize();  
-      });      
-    }
+    
   }; //end $.fn.stickable
 
 })(jQuery);
